1ec96b6f38f795b265f9d04ee44448f0ee31b0f6
root <root@Notebook.localdomain>
2022-07-26 18:03:20 +0200

    register

---
+++ app/migrations/Version20220726153611.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220726153611 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD first_name VARCHAR(20) NOT NULL, ADD last_name VARCHAR(30) NOT NULL, ADD city VARCHAR(40) NOT NULL, ADD company_name VARCHAR(255) NOT NULL, ADD address VARCHAR(255) NOT NULL, ADD phone_number INT NOT NULL, ADD pib INT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP first_name, DROP last_name, DROP city, DROP company_name, DROP address, DROP phone_number, DROP pib');
+    }
+}
